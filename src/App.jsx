5ab5b11094d35311134b5d72b9eeddd12e398eaa
Catherine Stahl <46716216+c-stahl@users.noreply.github.com>
2021-02-28 19:55:10 -0500

    Search for parking lots by location name

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+  const [search, setSearch] = useState();
+
+  // Save search bar input
+  const onInputChange = (e) => {
+    setSearch(e.target.value);
+  }
+
+  // Run the search query
+  const getData = async (searchString) => {
+    const searchUrl = encodeURI(searchString)
+
+    const response = await fetch(`https://api.yelp.com/v3/businesses/search?location=${searchUrl}&categories=parking`,
+      {
+        method: 'get', 
+        headers: {
+          'Authorization': 'Bearer mi5qSSqdhmrNXBjLq5MBMwuqcS0q8aE4u52fwqrG8CkrBjjksgdV8ZblHdh4ThtDqQVFapfOwrCqadcTH4sJIMhQgEcWpc0bK_9ms_rJ1H-xMT1Amp4tmH_PhAg3X3Yx'
+        }
+      }
+    );
+    const dataReturned = await response.json();
+    if(dataReturned.length > 0){
+      setData(dataReturned);
+      console.log(data);
+    }
+  };
+
+  const doSearch = () => {
+    getData(search);
+  };
+
+  // Main component
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Lots of Dislike</h1>
       </header>
+      <main>
+        <div className="search-bar">
+          <input value={search} onChange={onInputChange} type="search"></input>
+          <button onClick={doSearch}>Search</button>
+        </div>
+
+
+      </main>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,6 +3,19 @@ import { server, rest } from './setupTests'
 import App from './App';
 
 describe('The main page', () => { 
+  // Test that the search input is saved to state
+  // Test that the right data is retrieved and stored when the search button is pressed
+  // Test error handling from the api
+
+  // TODO: Show a list of parking lots from Yelp in that location with
+    // the address
+    // an image if available
+    // star rating
+    // review count
+    // link to the Yelp page.
+    // a parking lot score that factors in number of reviews and ratings
+      // using the formula: score = ( number of reviews * rating ) / (number of reviews + 1)
+
   it('renders a learn react link to reactjs.org', () => {  
     // Useful to set a mock response to specific statuses locally
     server.use(
